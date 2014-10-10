f17aa662f2da6ae176ab4f6b54e2aaf08460ee81
Will Han <xingheng907@hotmail.com>
2014-10-10 19:07:53 +0800

    Initial Commit

---
+++ Category_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Category_Demo
+//
+//  Created by WeiHan on 10/10/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Category_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Category_Demo
+//
+//  Created by WeiHan on 10/10/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
